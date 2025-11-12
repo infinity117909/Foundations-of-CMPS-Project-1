f2407c6aa8ae027d3caa7d6f0ade14dd4e2a22c8
Jimmy <jimmyscholz74@gmail.com>
2025-11-12 07:11:55 -0500

    Add basic TCP client application

---
+++ p1g1C.c
@@ -0,0 +1,51 @@
+// Basic TCP Client Application
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    const char *hello = "Hello from client";
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 address from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address / Address not supported");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send a message to the server
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // Read the server's response
+    int valread = read(sock, buffer, 1024);
+    buffer[valread] = '\0'; // Null-terminate the received string
+    printf("Message from server: %s\n", buffer);
+
+    // Close the socket
+    close(sock);
+    return 0;
+}
